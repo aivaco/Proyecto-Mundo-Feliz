1db942abeb9f6fb6dbcb827162f2f7a849bbe5f0
José Eduardo Picado Salas <joseps95@outlook.com>
2015-10-12 10:17:24 -0600

    Menú principal
    Agrega el encabezado, pie de página y pestañas que facilitan el acceso a
    los diferentes módulos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
-    
+  
+  # Controlador de la pagina con la vista principal
+  def home
+  end
+  
   def mientras
   end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages#mientras'       #Mientras
+  
+  root  'pages#mientras'       #Mientras
+  
+  get 'home' => 'pages#home'
+
+  
 end
