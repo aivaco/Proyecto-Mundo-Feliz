02903a204087c69298dba75c56647d656e534bd6
Jose Pablo <jpabloruizr@gmail.com>
2015-10-19 16:14:47 -0600

    Datos del cliente cargados

--- app/controllers/modulo_cliente_controller.rb
+++ app/controllers/modulo_cliente_controller.rb
@@ -1,6 +1,10 @@
 class ModuloClienteController < ApplicationController
 
   def modulo_cliente
+  	#@cliente = Cliente.find(params[:id])
+  	@cliente = {nombreEmp: "JP", ubicacion: "Guadalupe", contacto: "Ing. Ruiz", email: "jpabloruiz@gmail.com", telefono:"8870"}
+  	#@listaProyectos = Proyecto.find(@cliente.proyectos)
+  	@listaProyectos = [{nombre: "Proyecto 1"}, {nombre: "Proyecto 2"}, {nombre: "Proyecto 3"}, {nombre: "Proyecto 4"}, {nombre: "Proyecto 5"}]
   end
 
   def lista_clientes
