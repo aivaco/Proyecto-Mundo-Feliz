41cb6f6676fd77e8e260540fa16fef44ddf2da8c
William Eduardo Soto Martínez <williamsotomartinez@hotmail.com>
2015-11-02 05:43:25 +0000

    Update files

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,75 @@
+class SignupController < ApplicationController
+  
+  def new
+    
+  end
+
+  def create
+    
+    @persona=Persona.new(:idPersona=>params[:persona]["idPersona"], 
+                                            :email=>params[:persona]["email"],
+                                            :tipoPersona=>"f")
+    if @persona.save!
+      
+      @fisica=Fisica.new(:nombre=>params[:persona]["nombre"],
+                       :apellido1=>params[:persona]["apellido1"],
+                       :apellido2=>params[:persona]["apellido2"],
+                       :sexo=>"m",
+                       :fechaNac=>Date.new(params[:persona]["fechaNac(1i)"].to_i, params[:persona]["fechaNac(2i)"].to_i, params[:persona]["fechaNac(3i)"].to_i),
+                       :persona=>@persona)
+      if @fisica.save!
+        
+        @direccion=Direccion.new(:tipo=>"Principal",
+                                 :pais=>params[:persona]["pais"],
+                                 :provincia=>params[:persona]["provincia"],
+                                 :ciudad=>params[:persona]["ciudad"],
+                                 :dirExac=>params[:persona]["dirExac"],
+                                 :persona=>@persona)
+        if @direccion.save!
+          
+          @telefono=Telefono.new(:tipo=>"Principal",
+                                 :numero=>params[:persona]["numero"],
+                                 :persona=>@persona)
+          if @telefono.save!
+            
+            @user=User.new(:usuario=>params[:persona]["email"],
+                           :password=>params[:persona]["password"],
+                           :persona=>@persona)
+            if @user.save!
+              
+              redirect_to root_path  
+              return
+              
+            end
+            
+            flash.now[:danger] = 'Problemas guardando el usuario'
+            render 'new'
+            return
+            
+          end
+          
+          flash.now[:danger] = 'Problemas guardando el telefono'
+          render 'new'
+          return
+          
+        end
+        
+        flash.now[:danger] = 'Problemas guardando la direccion'
+        render 'new'
+        return
+        
+      end
+      
+      flash.now[:danger] = 'Problemas guardando los datos fisicos'
+      render 'new'
+      return
+      
+    end
+    
+    flash.now[:danger] = 'Problemas guardando la persona'
+    render 'new'
+    return
+    
+  end
+  
+end
