5c66449ceff3b962e19105e8b2a10ec9f425f6f8
William Eduardo Soto Martínez <williamsotomartinez@hotmail.com>
2015-11-04 23:19:58 +0000

    Fix login for unactivated accounts and add security check to New Client

--- app/controllers/new_client_controller.rb
+++ app/controllers/new_client_controller.rb
@@ -1,5 +1,14 @@
 class NewClientController < ApplicationController
   
+  before_action :logged_in_user
+  
+  def logged_in_user
+    unless logged_in?
+      #flash[:danger] = "Antes de acceder a cualquier sección, por favor inicie sesión."
+      redirect_to login_url
+    end
+  end
+  
   def new
     
   end
